233277fb753ce4b28714bbcb14dc225f2a8358f1
Bhavik Kothari <bhavikkothari@Bhaviks-MacBook-Pro.local>
2020-05-31 15:52:28 -0400

    Fully optimize to work with Realm

--- Tasky/Controllers/TaskyViewController.swift
+++ Tasky/Controllers/TaskyViewController.swift
@@ -7,7 +7,7 @@
 import UIKit
 import RealmSwift
 
-class TaskyViewController: UITableViewController {
+class TaskyViewController: SwipeTableViewController {
     
     var todoItems: Results<Item>?
     let realm = try! Realm()
@@ -31,7 +31,7 @@ class TaskyViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         if let item = todoItems?[indexPath.row] {
             cell.textLabel?.text = item.title
@@ -108,6 +108,19 @@ class TaskyViewController: UITableViewController {
         
         todoItems = selectedCategory?.items.sorted(byKeyPath: "title", ascending: true)
         tableView.reloadData()
+    }
+    
+    
+    override func updateModel(at indexPath: IndexPath) {
+        if let item = todoItems?[indexPath.row]{
+            do{
+            try realm.write{
+                realm.delete(item)
+            }
+            }catch{
+                print("Error deleting items \(error)")
+            }
+        }
     }
     
 }
